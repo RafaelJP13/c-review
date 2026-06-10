2b1c71af703ea84f604c0977f8ec54ebe2d3d008
Rafael Fernandes <rafaelsf013@gmail.com>
2026-06-10 11:43:10 -0300

    first commit

---
+++ first.c
@@ -0,0 +1,23 @@
+#includestdio.h
+
+int main(){
+    
+    float firstNumber, secondNumber;
+    float result;
+    
+    printf(Olá mundo!);
+    
+    printf(Digite o primeiro número );
+    scanf(%f, &firstNumber);
+    
+    printf(Digite o segundo número );
+    scanf(%f, &secondNumber);
+    
+    
+    result = firstNumber + secondNumber;
+    
+    printf(O resultado é %.2f, result);
+    
+    
+    return 0;
+}
